e5cce03b70a45de023fceb26d2a1b844c796b91f
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2017-06-22 14:29:19 +0530

    Added basic image caching utility

--- app/src/main/java/webops/shaastra/iitm/shaastra2018/MainActivity.java
+++ app/src/main/java/webops/shaastra/iitm/shaastra2018/MainActivity.java
@@ -1,10 +1,28 @@
 package webops.shaastra.iitm.shaastra2018;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.decode.BaseImageDecoder;
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+import com.nostra13.universalimageloader.core.imageaware.ImageViewAware;
+import com.nostra13.universalimageloader.utils.StorageUtils;
 
 import org.json.JSONObject;
+
+import java.io.File;
+
+import webops.shaastra.iitm.shaastra2018.imageCaching.ImageUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
         example: execute(url,p3.toString());
         will do the trick!
         */
+
+        String testImageURL = "http://placehold.it/240x120&text=image1";
+
+        ImageLoader imageLoader = ImageUtil.getImageLoader(this);
+
+
+        ImageView imview = (ImageView) findViewById(R.id.imView);
+
+        imageLoader.displayImage(testImageURL,imview);
     }
 
 }
---
+++ app/src/main/java/webops/shaastra/iitm/shaastra2018/imageCaching/ImageUtil.java
@@ -0,0 +1,55 @@
+package webops.shaastra.iitm.shaastra2018.imageCaching;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+/**
+ * Created by Rajat on 22-06-2017.
+ */
+
+public class ImageUtil {
+
+    public static void initImageLoader(Context context) {
+        // This configuration tuning is custom. You can tune every option, you may tune some of them,
+        // or you can create default configuration by
+        //  ImageLoaderConfiguration.createDefault(this);
+        // method.
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .memoryCacheSize(2 * 1024 * 1024)
+                .diskCacheSize(50 * 1024 * 1024)
+                .diskCacheFileCount(100)
+                .defaultDisplayImageOptions(defaultOptions)
+                .writeDebugLogs()
+                .build();
+
+        ImageLoader.getInstance().init(config);
+
+    }
+
+    /**
+     * Returns an ImageLoader instance assuming initImageLoader has already been called.
+     * @return ImageLoader instance
+     */
+    public static ImageLoader getImageLoader(){
+        return ImageLoader.getInstance();
+    }
+
+    /**
+     * Initializes the config of ImageLoader and returns an instance.
+     * @param context Context used in config init.
+     * @return
+     */
+    public static ImageLoader getImageLoader(Context context){
+        initImageLoader(context);
+        return ImageLoader.getInstance();
+    }
+}
