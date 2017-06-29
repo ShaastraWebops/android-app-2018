36e438cbdcf4944d44a301efcc50f19e7d484fd6
rohithram <ram234historyalex@gmail.com>
2017-06-29 17:12:22 +0530

    Added popup window and qr scanner code in mainactivity

--- app/src/main/java/webops/shaastra/iitm/shaastra2018/MainActivity.java
+++ app/src/main/java/webops/shaastra/iitm/shaastra2018/MainActivity.java
@@ -1,11 +1,30 @@
 package webops.shaastra.iitm.shaastra2018;
 
 import android.content.Context;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.CompoundBarcodeView;
 import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
@@ -32,15 +51,46 @@ import java.io.InputStream;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bt_qrscan;
+    //qr code scanner object
+    private IntentIntegrator qrScan;
+    boolean isClicked = true;
+    PopupWindow popUpWindow;
+    RelativeLayout mainLayout;
+    CardView containerLayout;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         ArrayList<Location> locs = new ArrayList<Location>();
         initLocations(locs);
+
+
+        mainLayout = (RelativeLayout)findViewById(R.id.rl_main);
+
+        containerLayout = (CardView) findViewById(R.id.cv_popup);
+
+        popUpWindow = new PopupWindow(MainActivity.this);
+
+        popUpWindow.setContentView(containerLayout);
+
+
+        bt_qrscan = (Button)findViewById(R.id.bt_qr_scan);
+
+
+
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
+        bt_qrscan.setOnClickListener(this);
 
         //Log.i("Json", String.valueOf(locs));
 
@@ -64,6 +114,8 @@ public class MainActivity extends AppCompatActivity {
         ImageView imview = (ImageView) findViewById(R.id.imView);
 
         imageLoader.displayImage(testImageURL,imview);
+
+
 
     }
 
@@ -105,4 +157,97 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //Getting the scan results
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        final String activity_name;
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                //converting the data to json
+
+                //setting values to activity_variable
+                activity_name = String.valueOf(result.getContents());
+                initiatePopupWindow(bt_qrscan,activity_name);
+
+
+
+
+            } }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void initiatePopupWindow(final View v, final String qr_response) {
+
+        try {
+            //We need to get the instance of the LayoutInflater, use the context of this activity
+
+            popUpWindow.setTouchable(true);
+            popUpWindow.setFocusable(true);
+
+            LayoutInflater inflater = (LayoutInflater)MainActivity.this
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            //Inflate the view from a predefined XML layout
+            View layout = inflater.inflate(R.layout.popup,
+                    (ViewGroup) findViewById(R.id.cv_popup));
+
+            final Button bt_dismiss_pop,bt_go;
+            TextView tv_qr_response;
+
+            tv_qr_response = (TextView) layout.findViewById(R.id.tv_qr_response);
+            bt_dismiss_pop = (Button)layout.findViewById(R.id.bt_pop_dismiss);
+            bt_go = (Button)layout.findViewById(R.id.bt_go);
+
+            tv_qr_response.setText(qr_response);
+
+
+
+
+            popUpWindow = new PopupWindow(layout,CardView.LayoutParams.WRAP_CONTENT,CardView.LayoutParams.WRAP_CONTENT,true);
+            // display the popup in the center
+            new Handler().postDelayed(new Runnable(){
+
+                public void run() {
+                    popUpWindow.showAtLocation( v, Gravity.CENTER, 0, 0);
+                }
+
+            }, 200L);
+
+            bt_dismiss_pop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                                popUpWindow.dismiss();
+
+
+                }
+            });
+            bt_go.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent navigate = new Intent(MainActivity.this,Main2Activity.class);
+                    navigate.putExtra("Activityname",qr_response);
+                    startActivity(navigate);
+                }
+            });
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    @Override
+    public void onClick(View v) {
+
+        qrScan.initiateScan();
+    }
 }
